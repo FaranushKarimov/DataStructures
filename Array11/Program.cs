7bbfe3a94f3b5e48ac01bd95d1de8f520b5ef8af
FaranushKarimov <faranush.karimov@gmail.com>
2022-01-06 14:04:19 +0300

    Array11 and Array12

---
+++ Array11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Array11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int k = Convert.ToInt32(Console.ReadLine());
+            if(k > n)
+            {
+                throw new Exception("Значение k не может быть больше n");
+            }
+            int[] array = new int[n];
+            for(int i = 1; i <= array.Length-1; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());  
+            }
+            for(int i = k; i <= array.Length-1; i+=k)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
---
+++ Array12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Array12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            if(n % 2 != 0)
+            {
+                throw new Exception("N должен быть чётным");
+            }
+            int[] array = new int[n];
+            for(int i = 0; i <= array.Length-1; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for(int i = 2; i <= array.Length-1; i+=2)
+            {
+                Console.WriteLine("Element[{0}] = {1}", i, array[i]);
+            }
+        }
+    }
+}
