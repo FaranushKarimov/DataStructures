f223c990e4f255d9f81823617376edbeb3be6d76
FaranushKarimov <faranush.karimov@gmail.com>
2022-01-06 16:14:08 +0300

    Array12 & Array13

---
+++ Array13/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Array13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            if(n % 2 == 0)
+            {
+                throw new Exception("N должен быть нечетным");
+            }
+            int[] array = new int[n+1];
+            for (int i = 1; i <= n;i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for(int i = n; i >= 1; i -= 2)
+            {
+                Console.WriteLine("Element[{0}] = {1}", i, array[i]);
+            }
+        }
+    }
+}
