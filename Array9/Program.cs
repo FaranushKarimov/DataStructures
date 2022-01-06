385d0c99a47d407462afdd1527622577baf7bcb0
FaranushKarimov <faranush.karimov@gmail.com>
2022-01-06 08:04:11 +0300

    Array9

---
+++ Array9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Array9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[n];
+            int count = 0;
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for(int j = array.Length - 1; j >= 0; j--)
+            {
+                if(array[j] % 2 == 0)
+                {
+                    count++;
+                    Console.Write(array[j] + " ");
+                }
+            }
+            Console.WriteLine("Количество четных чисел " + count);
+        }
+    }
+}
