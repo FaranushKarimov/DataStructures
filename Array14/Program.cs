76f96e3d70522522c8ad4163193df9a80f565723
FaranushKarimov <faranush.karimov@gmail.com>
2022-01-12 14:35:21 +0300

    Array14

---
+++ Array14/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Array14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[n];
+            for(int i = 0; i <= array.Length-1; ++i)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for(int i = 0; i <= array.Length-1; ++i)
+            {
+                if(i % 2 == 0)
+                {
+                    Console.Write(array[i]);
+                    Console.Write(" ");
+                }
+            }
+            for(int i = 0; i <= array.Length-1; ++i)
+            {
+                if(i % 2 != 0)
+                {
+                    Console.Write(array[i]);
+                    Console.Write(" ");
+                }
+            }
+        }
+    }
+}
