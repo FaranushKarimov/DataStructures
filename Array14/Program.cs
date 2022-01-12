1c8640830f01e02ff168acc987cd71a22fe2a16e
FaranushKarimov <faranush.karimov@gmail.com>
2022-01-12 15:32:50 +0300

    Array15

--- Array14/Program.cs
+++ Array14/Program.cs
@@ -7,26 +7,22 @@ namespace Array14
         static void Main(string[] args)
         {
             int n = Convert.ToInt32(Console.ReadLine());
-            int[] array = new int[n];
-            for(int i = 0; i <= array.Length-1; ++i)
+            int[] array = new int[n + 1];
+            for(int i = 1; i <= n; ++i)
             {
                 array[i] = Convert.ToInt32(Console.ReadLine());
             }
-            for(int i = 0; i <= array.Length-1; ++i)
+            for(int i = 2; i <= n; i += 2)
             {
-                if(i % 2 == 0)
-                {
                     Console.Write(array[i]);
                     Console.Write(" ");
-                }
+                
             }
-            for(int i = 0; i <= array.Length-1; ++i)
+            for(int i = 1; i <= n; i += 2)
             {
-                if(i % 2 != 0)
-                {
                     Console.Write(array[i]);
                     Console.Write(" ");
-                }
+        
             }
         }
     }
---
+++ Array15/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Array15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[n + 1];
+            for(int i = 1; i <= array.Length - 1; ++i)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for(int i = 1; i <= array.Length - 1; i += 2)
+            {
+                Console.Write(array[i]);
+                Console.Write(" ");
+            }
+            for(int i = n - Convert.ToInt32(array[n] % 2 != 0); i >= 1; i -= 2)
+            {
+                Console.Write(array[i]);
+                Console.Write(" ");
+            }
+        }
+    }
+}
