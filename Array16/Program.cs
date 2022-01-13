2ca26c925d0842303124fd99694270c404ee3e2e
FaranushKarimov <faranush.karimov@gmail.com>
2022-01-13 13:20:09 +0300

    Array16 & Array17

---
+++ Array16/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Array16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[n + 1];
+            for(int i = 0; i <= n - 1; ++i)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for(int i = 0, j = n - 1; i < n/2 && j >= n/2; ++i,--j)
+            {
+                    Console.Write($"{array[i]} {array[j]}  " );
+            }
+        }
+    }
+}
---
+++ Array17/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Array17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[n];
+            for(int i = 0; i <= n - 1; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for(int i = 0, j = n - 1; i <= n/4 && j >= n/4; i += 2, j -= 2)
+            {
+                Console.Write($"{array[i]} {array[i + 1]} {array[j]} {array[j - 1]} ");
+            }
+        }
+    }
+}
