05d395e940e9a5db9d0d4005b2e23684274c4928
FaranushKarimov <faranush.karimov@gmail.com>
2022-03-10 13:08:11 +0300

    SumOfArray

---
+++ Leetcode.RemoveDuplicatesFromSortedArray/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Leetcode.RemoveDuplicatesFromSortedArray
+{
+   
+    class Program
+    {
+        public static int[] RemoveDuplicates(int[] nums)
+        {
+            //int[] array = new int[nums.Length];
+            var array = nums.Distinct().ToArray();
+            //for (int i = 0; i < nums.Length; ++i)
+            //{
+            //    if (nums[i] != nums[i + 1])
+            //    {
+            //        array.Add(nums[i]);
+            //    }
+            //}
+            return array;
+        }
+        static void Main(string[] args)
+        {
+            var a = RemoveDuplicates(new int[] { 1, 2, 1 });
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SumOfArray/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'simpleArraySum' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY ar as parameter.
+     */
+
+    public static int simpleArraySum(List<int> ar)
+    {
+        int s = 0;
+        foreach (var item in ar)
+        {
+            s += item;
+        }
+        return s;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = Result.simpleArraySum(ar);
+        Console.WriteLine(result);
+
+        //textWriter.WriteLine(result);
+
+        //textWriter.Flush();
+        //textWriter.Close();
+    }
+}
