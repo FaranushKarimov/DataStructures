a3a2def79b552b2ce675f5e5041b0f6aeccc82fe
FaranushKarimov <faranush.karimov@gmail.com>
2022-04-11 16:47:03 +0300

    Array 17 & Array 18

---
+++ Array18/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Array18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            int[] array = new int[n];
+            for(int i = 0; i < 10; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for(int i = 0; i < 10; i++)
+            {
+                if(array[i] < array[n-1])
+                {
+                    Console.Write($"{array[i]}");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Array19/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Array19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = int.Parse(Console.ReadLine());
+            int n = 10;
+            int[] array = new int[n];
+            int result = 0;
+            for(int i = 0; i < 10; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for(int i = 0; i < 10; i++)
+            {
+                if(array[i] < array[k] && array[k] < array[n-1])
+                {
+                    //Console.Write($"{array[i]}");
+                    result = array[i];
+                    break;
+                } 
+                
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
