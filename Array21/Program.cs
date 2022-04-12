ae178ef24874a0889842ce8e6eed282fa6337593
FaranushKarimov <faranush.karimov@gmail.com>
2022-04-12 15:52:43 +0300

    Array21

---
+++ Array21/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Array21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            int l = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+        }
+    }
+}
