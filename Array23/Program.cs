f0ca5046519e5391bd2893adbb85f9cfe57cb3f1
FaranushKarimov <faranush.karimov@gmail.com>
2022-04-18 16:53:59 +0300

    Array 24

---
+++ Array23/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Array23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            int l = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            int sum = 0;
+            int count = 0;
+            for(int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            for(int i = 0; i < k - 1; i++)
+            {
+                sum += array[i];
+                count++;
+            }
+            for(int i = l; i < n; i++)
+            {
+                sum += array[i];
+                count++;
+            }
+            Console.WriteLine(sum / count);
+        }
+    }
+}
---
+++ Array24/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Array24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var a = new List<int>();
+            for(int i = 0; i < n; i++)
+            {
+                a.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            int D = a[1] - a[0];
+            for(int i = 1; i < n; i++)
+            {
+                int d = a[i] - a[i - 1];
+                if(d != D)
+                {
+                    Console.WriteLine(0);
+                    return;
+                }
+            }
+            Console.WriteLine(D);
+        }
+    }
+}
