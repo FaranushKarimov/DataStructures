1fcec7636dba4dcadd6df4fef45e03dfeb4660b0
FaranushKarimov <faranush.karimov@gmail.com>
2022-04-18 14:33:29 +0300

    Array 22

--- Array22/Program.cs
+++ Array22/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Array21
+namespace Array22
 {
     class Program
     {
@@ -11,17 +11,19 @@ namespace Array21
             int l = int.Parse(Console.ReadLine());
             int[] array = new int[n];
             int sum = 0;
-            int count = 0;
             for(int i = 0; i < n; i++)
             {
                 array[i] = int.Parse(Console.ReadLine());
             }
-            for(int i = k - 1; i < l; i++)
+            for(int i = 0; i < k - 1; i++)
             {
                 sum += array[i];
-                count++;      
             }
-            Console.WriteLine(sum / count);
+            for(int i = l; i < n; i++)
+            {
+                sum += array[i];
+            }
+            Console.WriteLine(sum);
         }
     }
 }
