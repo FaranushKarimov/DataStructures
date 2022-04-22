6c440e48ff6243e2f1685c37dfc9fcc2daec9ebb
FaranushKarimov <faranush.karimov@gmail.com>
2022-04-22 16:18:13 +0300

    Array 27 & Array 28

---
+++ Array27/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Array27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var a = new List<int>();
+            for(int i = 0; i < n; i++)
+            {
+                a.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            bool D = a[0] > 0 && a[1] < 0;
+            for(int i = 1; i < n; i += 2)
+            {
+                if((a[i-1] > 0 && a[i] < 0) != D)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+            }
+            Console.WriteLine(0);
+        }
+    }
+}
---
+++ Array28/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Array28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var a = new List<int>();
+            int min = 100000000;
+            for(int i = 0; i < n; i++)
+            {
+                a.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            for(int i = 1; i < n; i += 2)
+            {
+                if(a[i] < min)
+                {
+                    min = a[i];
+                }
+            }
+            Console.WriteLine(min);
+        }
+    }
+}
