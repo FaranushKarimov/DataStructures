f6a2d28a23c94f24e44806c166aa1cb762afac42
FaranushKarimov <faranush.karimov@gmail.com>
2022-04-27 14:20:30 +0300

    Array29 & Array30

---
+++ Array29/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Array29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var a = new List<int>();
+            int max = 0;
+            for(int i = 0; i < n; i++)
+            {
+                a.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            for(int i = 0; i < n; i += 2)
+            {
+                if(a[i] > max)
+                {
+                    max = a[i];
+                }
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
---
+++ Array30/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Array30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var a1 = new List<int>();
+            var a2 = new List<int>();
+            for(int i = 0; i < n; i++)
+            {
+                a1.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            for(int i = 0; i+1<n; i++)
+            {
+                if(a1[i] > a1[i + 1])
+                {
+                    a2.Add(i + 1);
+                }
+            }
+            Console.WriteLine(a2.Count);
+            foreach (var i in  a2)
+            {
+                Console.Write(i);
+                Console.Write(' ');
+            }
+        }
+    }
+}
